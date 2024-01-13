5ea2fa340a9bf817882c0af72cceff21f19bb403
andersbuck <buckles.anderson@gmail.com>
2024-01-13 15:26:14 -0500

    Change temp response text

--- src/handlers/find-one-company.ts
+++ src/handlers/find-one-company.ts
@@ -4,7 +4,7 @@ async function handler(event: APIGatewayProxyEvent, context: Context) {
   
   const response: APIGatewayProxyResult = {
     statusCode: 200,
-    body: JSON.stringify(`Hello from KarmaCart API!`),
+    body: JSON.stringify(`find-one-company success!`),
   };
 
   return response;
