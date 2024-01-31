f750d3614185c73ca91a556a3aa596ea463496c6
andersbuck <buckles.anderson@gmail.com>
2024-01-30 23:33:41 -0500

    Implement find one company

--- src/handlers/find-one-company.ts
+++ src/handlers/find-one-company.ts
@@ -1,13 +1,42 @@
 import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda";
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+import validator from 'validator';
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
 
 async function handler(event: APIGatewayProxyEvent, context: Context) {
 
-  
-  
-  const response: APIGatewayProxyResult = {
-    statusCode: 200,
-    body: JSON.stringify(`find-one-company id: ${event.pathParameters?.id} success!`),
-  };
+  const companyId = event.pathParameters?.id
+
+  if (!companyId) {
+    throw new Error('Company id is undefined')
+  }
+
+  validator.isNumeric(companyId)
+
+  const command = new GetCommand({
+    TableName: "Company",
+    Key: {
+      pk: `COMPANY#${companyId}`,
+    },
+  });
+
+  const getCommandOutput = await docClient.send(command);
+
+  let response: APIGatewayProxyResult
+  if (!getCommandOutput.Item) {
+    response = {
+      statusCode: 404,
+      body: 'Company not found',
+    };
+  } else {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify(getCommandOutput.Item),
+    };
+  }
 
   return response;
 }
