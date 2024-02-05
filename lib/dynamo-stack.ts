82c201645bdbacaee8c9a843a56f2ae3d9b77322
andersbuck <buckles.anderson@gmail.com>
2024-02-05 00:51:30 -0500

    Add seed data for db

--- lib/dynamo-stack.ts
+++ lib/dynamo-stack.ts
@@ -1,7 +1,9 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
+import { RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
 import * as dynamo from 'aws-cdk-lib/aws-dynamodb';
+import * as customResources from 'aws-cdk-lib/custom-resources'
 import { Construct } from 'constructs';
 import { COMPANY_TABLE } from '../src/const/dynamo.const';
+import { seedData } from '../seed-data/seed-data';
 
 /**
  * This Stack creates the Dynamo DB for the Karmacart API.
@@ -21,6 +23,24 @@ export class DynamoStack extends Stack {
       billingMode: dynamo.BillingMode.PROVISIONED,
       readCapacity: 5,
       writeCapacity: 1,
+      removalPolicy: RemovalPolicy.RETAIN_ON_UPDATE_OR_DELETE
+    });
+
+    const awsSdkCall: customResources.AwsSdkCall = {
+      service: 'DynamoDB',
+      action: 'batchWriteItem',
+      parameters: {
+          RequestItems: { 
+            CompanyTable: seedData
+          }
+      },
+      physicalResourceId: customResources.PhysicalResourceId.of(this.companyTable.tableName + '_initialization')
+    }
+
+    new customResources.AwsCustomResource(this, 'initTable', {
+      onUpdate: awsSdkCall,
+      onCreate: awsSdkCall,
+      policy: customResources.AwsCustomResourcePolicy.fromSdkCalls({ resources: customResources.AwsCustomResourcePolicy.ANY_RESOURCE }),
     });
   }
 }
---
+++ seed-data/seed-data.ts
@@ -0,0 +1,86 @@
+export const seedData = [
+  {
+    PutRequest: {
+      Item: {
+        pk: { S: "COMPANY#0000000000000" },
+        address: { S: "1234 Smallville USA" },
+        annualRevenue: { S: "1000000" },
+        companyName: { S: "Example Low Score" },
+        ethicalScore: { N: "2" },
+        website: { S: "https://www.ethicalconsumer.org/" }
+      }
+    }
+  },
+  {
+    PutRequest: {
+      Item: {
+        pk: { S: "COMPANY#0000000000001" },
+        address: { S: "2345 Medville USA" },
+        annualRevenue: { S: "1000000" },
+        companyName: { S: "Example Med Score" },
+        ethicalScore: { N: "7" },
+        website: { S: "https://www.ethicalconsumer.org/" }
+      }
+    }
+  },
+  {
+    PutRequest: {
+      Item: {
+        pk: { S: "COMPANY#0000000000002" },
+        address: { S: "3456 Largville USA" },
+        annualRevenue: { S: "1000000" },
+        companyName: { S: "Example High Score" },
+        ethicalScore: { N: "15" },
+        website: { S: "https://www.ethicalconsumer.org/" }
+      }
+    }
+  },
+  {
+    PutRequest: {
+      Item: {
+        pk: { S: "COMPANY#0817939012390" },
+        address: { S: "12 York Gate London, NW1 4QS, UK" },
+        annualRevenue: { S: "21600000" },
+        companyName: { S: "Method Products Ltd" },
+        ethicalScore: { N: "4" },
+        website: { S: "https://www.methodproducts.co.uk" }
+      }
+    }
+  },
+  {
+    PutRequest: {
+      Item: {
+        pk: { S: "COMPANY#0048256296181" },
+        address: { S: "Astonish House, Unit 1, Premier point, Staithgate Lane, Bradford BD6 1DW, UK" },
+        annualRevenue: { S: "27500000" },
+        companyName: { S: "Astonish Holdings Ltd" },
+        ethicalScore: { N: "12" },
+        website: { S: "https://www.astonishcleaners.co.uk/" }
+      }
+    }
+  },
+  {
+    PutRequest: {
+      Item: {
+        pk: { S: "COMPANY#0000000000003" },
+        address: { S: "1 Coca-Cola Plaza NW Atlanta, GA 30313, USA" },
+        annualRevenue: { S: "25000000000" },
+        companyName: { S: "The Coca-Cola Company" },
+        ethicalScore: { N: "1" },
+        website: { S: "https://www.coca-colacompany.com/" }
+      }
+    }
+  },
+  {
+    PutRequest: {
+      Item: {
+        pk: { S: "COMPANY#0815796020008" },
+        address: { S: "Timsons Business Centre, Bath Road, Kettering Northamptonshire NN16 8NQ, UK" },
+        annualRevenue: { S: "80326" },
+        companyName: { S: "Cawston Press Ltd" },
+        ethicalScore: { N: "12" },
+        website: { S: "https://cawstonpress.com/" }
+      }
+    }
+  }
+]
